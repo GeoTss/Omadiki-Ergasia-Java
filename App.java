615c28ec8a63fce5052706665e01eac4378df5de
Toto <filippostotomis2@gmail.com>
2023-06-01 21:42:44 +0300

    pwpw fagame kai shmera

--- App.java
+++ App.java
@@ -23,28 +23,28 @@ public class App {
     _productList.insertProduct(new Product("World", "null", "2069"));
     _productList.insertProduct(new Product("ARDVARG", "Pays off", "1821"));
     typeList _typeList = new typeList();
-    _typeList.insertType(new PrintedAdType("get rekt", 3.14f, 6.28f, 9.42f));
-    _typeList.insertType(new PrintedAdType("I am not a god, i am not a king, i am... worse", 3.03f, 8.18f, 10.02f));
-    _typeList.insertType(new PrintedAdType("frontises smr", 15f, 60f, 28f));
-    _typeList.insertType(new RadioTVAdType("o pappas o paxus", 1f, 2f, 3f, 4f));
-    _typeList.insertType(new RadioTVAdType("paei o palios o xronos", 2f, 4f, 6f, 8f));
-    _typeList.insertType(new RadioTVAdType("ela re ti leei", 5f, 10f, 15f, 20f));
-    _typeList.insertType(new WebAdType("Giannhs", 2f, 6f, 10f));
-    _typeList.insertType(new WebAdType("Giannakhs", 29f, 43f, 10f));
-    _typeList.insertType(new WebAdType("Giannaras", 8f, 11.880f, 9.11f));
-    adList _AdList = new adList();
-    _AdList.insertAd(new PrintedAd());
-    _AdList.insertAd(new PrintedAd());
-    _AdList.insertAd(new PrintedAd());
-    _AdList.insertAd(new PrintedAd());
-    _AdList.insertAd(new RadioTVAd());
-    _AdList.insertAd(new RadioTVAd());
-    _AdList.insertAd(new RadioTVAd());
-    _AdList.insertAd(new RadioTVAd());
-    _AdList.insertAd(new WebAd());
-    _AdList.insertAd(new WebAd());
-    _AdList.insertAd(new WebAd());
-    _AdList.insertAd(new WebAd());
+    _typeList.insertType(new PrintedAdType("get rekt","1234", 3.14f, 6.28f, 9.42f));
+    _typeList.insertType(new PrintedAdType("I am not a god, i am not a king, i am... worse", "1234", 3.03f, 8.18f, 10.02f));
+    _typeList.insertType(new PrintedAdType("frontises smr", "1234", 15f, 60f, 28f));
+    _typeList.insertType(new RadioTVAdType("o pappas o paxus", "5678", 1f, 2f, 3f, 4f));
+    _typeList.insertType(new RadioTVAdType("paei o palios o xronos", "5678",2f, 4f, 6f, 8f));
+    _typeList.insertType(new RadioTVAdType("ela re ti leei", "5678", 5f, 10f, 15f, 20f));
+    _typeList.insertType(new WebAdType("Giannhs", "6969", 2f, 6f, 10f));
+    _typeList.insertType(new WebAdType("Giannakhs", "8272", 29f, 43f, 10f));
+    _typeList.insertType(new WebAdType("Giannaras", "2817", 8f, 11.880f, 9.11f));
+    adList _adList = new adList();
+    _adList.insertAd(new PrintedAd((PrintedAdType) _typeList.get(0), 7548, "kys", 10, 100, "middle"));
+    _adList.insertAd(new PrintedAd((PrintedAdType) _typeList.get(1), 0420, "syk", 8, 105, "front"));
+    _adList.insertAd(new PrintedAd((PrintedAdType) _typeList.get(2), 8483, "i have commited several warcrimes", 5, 50, "back"));
+    _adList.insertAd(new PrintedAd((PrintedAdType) _typeList.get(2), 7543, "no reason", 100, 2, "middle"));
+    _adList.insertAd(new RadioTVAd((RadioTVAdType) _typeList.get(4), 2134, "i just had to do it", 10, "morning", 60));
+    _adList.insertAd(new RadioTVAd((RadioTVAdType) _typeList.get(5), 1234, "i am tired of typing", 30, "midday", 10));
+    _adList.insertAd(new RadioTVAd((RadioTVAdType) _typeList.get(5), 9898, "the mentor approves", 101, "evening", 610));
+    _adList.insertAd(new RadioTVAd((RadioTVAdType) _typeList.get(4), 20921, "madclip leipeis", 1, "night", 17));
+    _adList.insertAd(new WebAd((WebAdType) _typeList.get(8), 0210, "javac App.java", 10, true, 2));
+    _adList.insertAd(new WebAd((WebAdType) _typeList.get(6), 8473, "java App", 11, false, 10));
+    _adList.insertAd(new WebAd((WebAdType) _typeList.get(7), 1312, "App.java:44: error: constructor WebAd in class WebAd cannot be applied to given types;", 101, true, 22));
+    _adList.insertAd(new WebAd((WebAdType) _typeList.get(8), 9584, "insert lenny face here", 165, false, 80));
     int answer = getResponse();
     while ((answer > 0) && (answer <= 9)) {
       Scanner scan = new Scanner(System.in);
@@ -52,21 +52,56 @@ public class App {
         case 1:
           System.out.println("Insert VAT and Name of the new Carrier");
           _carrierList.insertCarrier(new Carrier(scan.next(),scan.next()));
+          break;
         case 2:
-          System.out.println();
+          System.out.println("Insert 1 for PrintedAd, 2 for RadioTVAd , 3 for WebAd");
+          switch (scan.nextInt()) {
+            case 1:
+              System.out.println("Insert description, VAT of carrier, cost for the front page, cost for the middle page and cost for the back page.");
+              _typeList.insertType(new PrintedAdType(scan.next(), scan.next(), scan.nextFloat(), scan.nextFloat(), scan.nextFloat()));
+              break;
+            case 2:
+              System.out.println("Insert description, VAT of carrier, cost per second in the morning, cost per second in the midday, cost per second in the evening and cost per second in the night.");
+              _typeList.insertType(new RadioTVAdType(scan.next(), scan.next(), scan.nextFloat(), scan.nextFloat(), scan.nextFloat(), scan.nextFloat()));
+              break;
+            case 3:
+              System.out.println("Insert description, VAT of carrier, cost per day, cost per additional page and cost of auto show.");
+              _typeList.insertType(new WebAdType(scan.next(), scan.next(), scan.nextFloat(), scan.nextFloat(), scan.nextFloat()));
+              break;
+          }
+          break;
         case 3:
+          System.out.println("Insert 1 for PrintedAd, 2 for RadioTVAd , 3 for WebAd");
+          switch (scan.nextInt()) {
+            case 1:
 
+              break;
+            case 2:
+          
+              break;
+            case 3:
+
+              break;
+          }
+          break;
         case 4:
-        
+          _adList.displayAdList();
+          break;
         case 5:
         
+          break;
         case 6:
 
+          break;
         case 7:
-        
+          
+          break;
         case 8:
-
+          
+          break;
         case 9:
+          
+          break;
       }
       scan.close();
       getResponse();
--- typeList.java
+++ typeList.java
@@ -4,5 +4,8 @@ public class typeList {
     public void insertType(AdvertisementType _AdvertisementType) {
         myArray.add(_AdvertisementType);
     }
+    public AdvertisementType get(int index) {
+        return myArray.get(index);
+    }
 }
 
